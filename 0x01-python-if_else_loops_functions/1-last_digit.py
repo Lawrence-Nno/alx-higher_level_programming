c5f5cf51a84c474b87441e1781dd088f85c22e32
Lawrence <lawrence.nno@gmail.com>
2022-11-29 03:27:57 -0800

    Added 1-last_digit.py file

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -9,8 +9,11 @@ if number < 0:
 elif number >= 0:
     a = number % 10
     if a > 5:
-        print("Last digit of " + str(o) + " is " + str(a) + " and is greater than 5")
+        print("Last digit of " + str(o) + " is "/
+                + str(a) + " and is greater than 5")
     elif a == 0:
-        print("Last digit of " + str(o) + " is " + str(a) + " and is 0")
+        print("Last digit of " + str(o) + " is "/
+                + str(a) + " and is 0")
     else:
-        print("Last digit of " + str(o) + " is " + str(a) + " and is less than 6 and not 0")
+        print("Last digit of " + str(o) + " is "/
+                + str(a) + " and is less than 6 and not 0")
