ef603c617c79e8c33f144f930d7b6f0785236ecf
Lawrence <lawrence.nno@gmail.com>
2022-11-29 04:13:05 -0800

    Added 5-print_comb2.py file

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -3,4 +3,4 @@ for i in range(0, 100):
     if i == 99:
         print('{0}{1}'.format((i//10), (i % 10)))
     else:
-        print('{0}{1}, '.format((i//10), (i % 10)),end="")
+        print('{0}{1}, '.format((i//10), (i % 10)), end="")
