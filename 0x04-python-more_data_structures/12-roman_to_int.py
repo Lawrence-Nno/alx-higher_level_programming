a6ed57a1a478476b5173eec444148602c1b8c42f
Lawrence <lawrence.nno@gmail.com>
2022-12-07 05:33:51 -0800

    Added 12-roman_to_int.py file

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -25,6 +25,6 @@ def roman_to_int(roman_string):
             num += roman_dictionary[roman_string[i]] * -1
 
         else:
-            num += roman_dict[roman_string[i]]
+            num += roman_dictionary[roman_string[i]]
 
         return (num)
