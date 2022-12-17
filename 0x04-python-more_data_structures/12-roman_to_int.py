2f58e7a587d3351f680d5437af710e16d37bc6a0
Lawrence <lawrence.nno@gmail.com>
2022-12-17 02:30:12 -0800

    Corrected 12-roman_to_int.py file

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,32 +1,45 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    val = 0
-    count = 0
-    length = len(roman_string)
-    roman_dict = {
-        'I': 1,
-        'V': 5,
-        'X': 10,
-        'L': 50,
-        'C': 100,
-        'D': 500,
-        'M': 1000
-    }
-
+    # Fail checks, none, not a string
+    if not roman_string:
+        return 0
     if not isinstance(roman_string, str):
         return 0
-    for a in list(roman_string):
-        if a not in roman_dict:
-            return 0
+    if not roman_string.isupper():
+        return 0
+    # Dictionary for roman numerals
+    r_dict = {
+        "I": 1,
+        "IV": 4,
+        "V": 5,
+        "IX": 9,
+        "X": 10,
+        "L": 50,
+        "C": 100,
+        "D": 500,
+        "M": 1000
+    }
 
-    for a in list(roman_string):
-        if count < length - 1:
-            if roman_dict[list(roman_string)[count]] < roman_dict[list(roman_string)[count + 1]]:
-             val -= roman_dict[a]
-            else:
-                val += roman_dict[a]
-        else:
-            val += roman_dict[a]
-        count = count + 1
-
-    return val
+    result = 0
+    temp = list(roman_string)
+    # Concat 4 and 9s
+    if len(temp) > 1:
+        idx = 0
+        for i in temp:
+            try:
+                if temp[idx] == 'I' and temp[idx + 1] == 'V':
+                    temp[idx:idx + 2] = [''.join(temp[idx:idx + 2])]
+            except IndexError:
+                pass
+            try:
+                if temp[idx] == 'I' and temp[idx + 1] == 'X':
+                    temp[idx:idx + 2] = [''.join(temp[idx:idx + 2])]
+            except IndexError:
+                pass
+            idx += 1
+    # Search in dict for correct numbers and add
+    for k, v in r_dict.items():
+        for index in temp:
+            if index == k:
+                result += v
+    return result
