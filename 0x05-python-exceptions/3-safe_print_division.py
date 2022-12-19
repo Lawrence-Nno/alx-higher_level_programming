0bbfc1c8206e2e57ef034487c13306cbd6383c53
Lawrence <lawrence.nno@gmail.com>
2022-12-19 05:10:26 -0800

    Added Task 3 files

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,9 +1,15 @@
 #!/usr/bin/python3
+
+
 def safe_print_division(a, b):
+    """
+    divides two integers and prints the result
+    catches divide by zero exception
+    """
     try:
-       c = a / b
-    except (TypeError, ZeroDivisionError):
-        c = None
+        res = a / b
+    except:
+        res = None
     finally:
-        print("Inside result: {}".format(c))
-    return c
+        print("Inside result: {}".format(res))
+    return res
