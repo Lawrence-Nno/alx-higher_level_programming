886b0e6c7b27d9a45c3e23d2580b023bfa58a61b
Lawrence Nno <Lawrence.Nno@gmail.com>
2023-01-03 22:23:46 +0100

    Added Advanced Task 101 files

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Represents a locked class"""
+class LockedClass:
+    """This class prevents new objects/instances to be created
+    else its name is "first_name"""
+
+    __slots__ = ["first_name"]
