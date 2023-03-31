98c3a2b381bcbec42b5025959b3069dfc3814b44
Lawrence Nno <Lawrence.Nno@gmail.com>
2023-03-31 18:46:37 +0100

    Added Task 100

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 """
-list 10 commits (from the most recent to oldest) of the repository and user
-sent in as arguments
+This script lists 10 commits (from the most recent to the oldest) of 
+the repository and user sent in as arguments
 """
 if __name__ == '__main__':
     import requests
     from sys import argv
-    reqst = requests.get('https://api.github.com/repos/{}/{}/commits'
+    req = requests.get('https://api.github.com/repos/{}/{}/commits'
                      .format(argv[2], argv[1]))
-    commits = reqst.json()
+    commits = req.json()
     for commit in commits[:10]:
         print(commit.get('sha'), end=': ')
         print(commit.get('commit').get('author').get('name'))
