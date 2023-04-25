6998fa7d8e6506e36ca90c5efb9481b527754144
Lawrence Nno <Lawrence.Nno@gmail.com>
2023-04-25 16:16:39 +0100

    Fixed Task 101

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,16 +1,16 @@
 #!/usr/bin/node
 const request = require('request');
 const url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
-request(url, function (error, response, body) {
-  if (!error) {
+request(url, function (err, response, body) {
+  if (!err) {
     let characters = JSON.parse(body).characters;
     printCharacters(characters, 0);
   }
 });
 
 function printCharacters (characters, index) {
-  request(characters[index], function (error, response, body) {
-    if (!error) {
+  request(characters[index], function (err, response, body) {
+    if (!err) {
       console.log(JSON.parse(body).name);
       if (index + 1 < characters.length) {
         printCharacters(characters, index + 1);
