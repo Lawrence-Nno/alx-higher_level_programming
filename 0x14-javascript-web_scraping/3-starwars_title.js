09a5c90cb00cc426d9485eb124e0bdb5d2b77135
Lawrence Nno <Lawrence.Nno@gmail.com>
2023-04-25 14:14:46 +0100

    Fixed Task 3

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const request = require('request');
-let url = 'https://swapi-api.alx-tools.com/api/films/:id' + process.argv[2];
+let url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
 request(url, function (err, response, body) {
   console.log(err || JSON.parse(body).title);
 });
